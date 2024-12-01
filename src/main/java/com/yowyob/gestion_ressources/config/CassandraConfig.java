f1d7d67cd010891b2670718a8b768c7d082c0a13
z1004-2002 <zogningfranc@gmail.com>
2024-12-01 04:57:32 +0100

    end

--- src/main/java/com/yowyob/gestion_ressources/config/CassandraConfig.java
+++ src/main/java/com/yowyob/gestion_ressources/config/CassandraConfig.java
@@ -1,5 +1,6 @@
 package com.yowyob.gestion_ressources.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
@@ -7,25 +8,33 @@ import org.springframework.data.cassandra.config.SchemaAction;
 @Configuration
 // @EnableCassandraRepositories(basePackages = "com.yowyob.gestion_ressources.infrastructure.persistence.repository")
 public class CassandraConfig extends AbstractCassandraConfiguration {
+    @Value("${spring.cassandra.keyspace-name}")
+    private String keySpaceName;
+    @Value("${spring.cassandra.contact-points}")
+    private String contactNamePoint;
+    @Value("${spring.cassandra.port}")
+    private int port;
+    @Value("${spring.cassandra.local-datacenter:}")
+    private String localDataCenter;
 
     @Override
     protected String getKeyspaceName() {
-        return "yowyobdb";
+        return this.keySpaceName;
     }
 
     @Override
     public String getContactPoints() {
-        return "127.0.0.1";
+        return this.contactNamePoint;
     }
 
     @Override
     protected int getPort() {
-        return 9042;
+        return this.port;
     }
 
     @Override
     public String getLocalDataCenter() {
-        return "datacenter1";
+        return this.localDataCenter;
     }
 
     @Override
